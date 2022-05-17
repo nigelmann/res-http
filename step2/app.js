0186d93021803b60c2e30282c6490a0bb1c377c4
Simon Ackermann <simon@inthebox.ch>
2022-05-17 20:14:14 +0200

    [BUGFIX/FEATURE] fix node version & add now route

--- step2/app.js
+++ step2/app.js
@@ -3,13 +3,20 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({message: 'Hello World!'}))
+})
+
+app.get('/now', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({now: Date.now()}))
 })
 
 app.get('/:name', (req, res) => {
-  res.send(`Hello ${req.params.name}`)
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({message: `Hello ${req.params.name}`}))
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Dynamic app listening on port ${port}`)
 })
