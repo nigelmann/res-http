a5566c81043951bae2d7df679f8d1df882e4efaa
Noah Boegli <39568628+noahboegli@users.noreply.github.com>
2022-05-24 21:21:51 +0200

    [FEATURE] le const

--- dynamic/app.js
+++ dynamic/app.js
@@ -1,20 +1,21 @@
 const express = require('express')
+const os = require("os");
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({message: 'Hello World!'}))
+    res.send(JSON.stringify({message: 'Hello World!', hostname: os.hostname()}))
 })
 
 app.get('/now', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({now: Date.now()}))
+    res.send(JSON.stringify({now: Date.now(), hostname: os.hostname()}))
 })
 
 app.get('/:name', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({message: `Hello ${req.params.name}`}))
+    res.send(JSON.stringify({message: `Hello ${req.params.name}`, hostname: os.hostname()}))
 })
 
 app.listen(port, () => {
