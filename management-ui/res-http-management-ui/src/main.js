90bec1f01a64c97b89650d91ed344532683058b4
Noah Boegli <39568628+noahboegli@users.noreply.github.com>
2022-05-25 23:59:04 +0200

    [FEATURE] Tuned management console to the max

--- management-ui/res-http-management-ui/src/main.js
+++ management-ui/res-http-management-ui/src/main.js
@@ -4,8 +4,11 @@ import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
 const app = createApp(App)
 
 app.use(router)
-
+app.use(VueSweetalert2);
 app.mount('#app')
--- management-ui/res-http-management-ui/src/views/HomeView.vue
+++ management-ui/res-http-management-ui/src/views/HomeView.vue
@@ -18,6 +18,32 @@ export default {
     },
     doFetch: function (url, params) {
       return fetch(url, params);
+    },
+    getLabelsString: function (labelsArray) {
+      return Object.keys(labelsArray).map(key => `<strong>${key}</strong> => ${labelsArray[key]}`).join('<br>')
+    },
+    getMountsString: function (mountsArray) {
+      let string = "";
+      let i = 0;
+      for (const mount of mountsArray) {
+        string += `<h3>#${i++}</h3>` + Object.keys(mount).map(key => `<strong>${key}</strong> => ${mount[key]}`).join('<br>') + "<hr>";
+      }
+
+      // Removing last <hr>, yes I'm a lazy wanker
+      string = string.slice(0, -4);
+      return string;
+    },
+    getPortsMapping: function (portsMappingArray) {
+
+      let string = "";
+      let i = 0;
+      for (const portMapping of portsMappingArray) {
+        string += `<h3>#${i++}</h3>` + Object.keys(portMapping).map(key => `<strong>${key}</strong> => ${portMapping[key]}`).join('<br>') + "<hr>";
+      }
+
+      // Removing last <hr>, yes I'm a lazy wanker again
+      string = string.slice(0, -4);
+      return string;
     }
   },
   mounted() {
@@ -44,6 +70,20 @@ export default {
 <template>
   <main>
     <div class="container mt-5">
+      <hr />
+      <p>
+        On this page you can view the containers that have been run at least once (and not removed!) for the "res-http" project.
+        You may stop or start them.
+        <br />Due to the limitations of the docker socket API without using swarms, containers cannot be scaled (there is no API for docker-compose!)
+      </p>
+      <p>
+        Scaling can be done easily on the host running docker with a command like this one
+        <br />
+        <kbd>docker-compose up -d --scale static=5 --no-recreate</kbd> or
+        <kbd>docker-compose up -d --scale dynamic=5 --no-recreate</kbd>
+      </p>
+      <p>This page refreshes data automatically, so if you scale the infrastructure, no need to restart or even refresh the management page.</p>
+      <hr />
       <h1>Containers</h1>
       <div class="row mb-5" v-for="container in containers">
         <div class="col-md-12">
@@ -62,10 +102,20 @@ export default {
                 <br />
                 <strong>Command</strong>:
                 <kbd>{{ container.Command }}</kbd>
+                <br />
+                <strong>Mounts count</strong>
+                :
+                {{ container.Mounts?.length ?? 0 }}
+                <br />
+                <strong>Ports mapping (IPv4 & IPv6 included)</strong>
+                :
+                {{ container.Ports?.length ?? 0 }}
               </p>
-              <!--<p
-                class="card-text"
-              >With supporting text below as a natural lead-in to additional content.</p>-->
+              <div
+                class="alert alert-danger"
+                v-if="container.Names[0].includes('management')"
+                role="alert"
+              >Warning: This is the management container. If you stop it, the management console will stop responding, use with caution.</div>
               <button
                 @click="stopContainer($event, container.Id)"
                 class="btn btn-danger"
@@ -76,6 +126,23 @@ export default {
                 class="btn btn-success"
                 v-else
               >Start container</button>
+              &nbsp;&nbsp;
+              <button
+                class="btn btn-info"
+                @click="this.$swal({ width: '90%', html: this.getLabelsString(container.Labels) })"
+              >Show container labels</button>
+              &nbsp;&nbsp;
+              <button
+                class="btn btn-info"
+                @click="this.$swal({ width: '90%', html: this.getMountsString(container.Mounts) })"
+                v-if="container.Mounts.length > 0"
+              >Show mounts</button>
+              &nbsp;&nbsp;
+              <button
+                class="btn btn-info"
+                @click="this.$swal({ width: '90%', html: this.getPortsMapping(container.Ports) })"
+                v-if="container.Ports.length > 0"
+              >Show ports mapping</button>
             </div>
           </div>
         </div>
@@ -84,3 +151,12 @@ export default {
   </main>
 </template>
 
+<style>
+.swal2-html-container {
+  text-align: left !important;
+}
+.swal2-actions {
+  justify-content: flex-end !important;
+  width: 98% !important;
+}
+</style>
